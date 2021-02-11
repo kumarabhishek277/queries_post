d03f17950dfc5668f041904a1262334bb0abdfa6
Kumar Abhishek <abhish277kumar@gmail.com>
2021-02-11 18:43:58 +0530

    initial-commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+
+const ejs = require('ejs')
+
+const path = require('path')
+
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT|| 3000;
+app.use(express.static('public'))
+app.use(express.json())//to tell express that we are using json data
+app.use(express.urlencoded({extended:false}))
+
+app.get('/',(req,res)=>{
+    return res.render('home');
+})
+
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/views'))
+
+app.set('view engine', 'ejs')
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Listening at port ${PORT}`);
+})
